dc4becf415c9af52b81a1148cffe3b25ead24238
imeureka <lky062@naver.com>
2024-03-08 01:38:08 +0900

    feat: 검색창 1차 구현

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,8 +1,22 @@
+import BookList from './bookList/BookList';
+import Input from './input/Input';
+import styled from 'styled-components';
+
+const SearchWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  position: absolute;
+  top: 15%;
+  align-items: center;
+`;
 
 export default function Search() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <SearchWrapper>
+        <Input />
+        <BookList />
+      </SearchWrapper>
+    </>
+  );
 }
