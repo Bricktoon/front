9f71cb891c0773d8d8a03511a63453ffdbd5ae96
imeureka <lky062@naver.com>
2024-03-08 01:37:55 +0900

    feat: 헤더 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,3 +1,44 @@
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 80px;
+  background-color: #191769;
+  color: white;
+  position: fixed;
+  top: 0;
+
+  z-index: 1000;
+`;
+
+const HeaderContent = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1rem;
+`;
+
+const Logo = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`;
+
+const Location = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`;
+
 export default function Header() {
-  return <div>헤더입니다</div>;
+  return (
+    <HeaderWrapper>
+      <HeaderContent>
+        <Location>대학로점</Location>
+        <Logo>콩툰</Logo>
+      </HeaderContent>
+    </HeaderWrapper>
+  );
 }
