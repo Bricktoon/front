b3923d37ec71a58a47e8bb956afb7ca48b877d60
imeureka <lky062@naver.com>
2024-03-08 13:00:44 +0900

    feat: 토큰 검사

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,20 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import MainPage from '../pages/MainPage';
 import HeaderLayout from '../layouts/HeaderLayout';
 import AdminPage from '../pages/AdminPage';
 import LoginPage from '../pages/LoginPage';
+
+// 로컬스토리지에서 토큰을 가져오는 함수
+const getTokenFromLocalStorage = () => {
+  return localStorage.getItem('token');
+};
+
+// 토큰이 있는지 확인하는 함수
+const isTokenExist = () => {
+  const token = getTokenFromLocalStorage();
+  return !!token;
+};
 
 const router = createBrowserRouter([
   {
@@ -19,11 +30,11 @@ const router = createBrowserRouter([
         children: [
           {
             path: '/',
-            element: <MainPage />,
+            element: isTokenExist() ? <MainPage /> : <Navigate to='/login' />,
           },
           {
             path: '/admin',
-            element: <AdminPage />,
+            element: isTokenExist() ? <AdminPage /> : <Navigate to='/login' />,
           },
         ],
       },
