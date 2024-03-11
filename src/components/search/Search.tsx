a33f007b3b2236b6dcfa12c46a5db0369425e391
imeureka <lky062@naver.com>
2024-03-11 22:57:45 +0900

    chore: 안쓰는 부분 주석 처리

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,15 +1,15 @@
 import Input from './input/Input';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 export default function Search() {
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    // 로그아웃 로직 구현
-    localStorage.removeItem('accessToken');
-    navigate('/');
-    // 로그아웃 후에는 로그인 페이지로 이동하도록 설정할 수 있습니다.
-  };
+  // const navigate = useNavigate();
+  // const handleLogout = () => {
+  //   // 로그아웃 로직 구현
+  //   localStorage.removeItem('accessToken');
+  //   navigate('/');
+  //   // 로그아웃 후에는 로그인 페이지로 이동하도록 설정할 수 있습니다.
+  // };
 
   return (
     <>
